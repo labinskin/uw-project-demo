8e153d12eff09e256f5fb587558fc30a313c4ad8
Breanna Frankenberg <frankenbergbreanna@rebusenergysolutions.com>
2022-02-06 16:43:46 -0700

    uploading ERD documents and updated readme

---
+++ QuickDBD-schema.sql
@@ -0,0 +1,45 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Offenses" (
+    "county" varchar   NOT NULL,
+    "agency_name" varchar   NOT NULL,
+    "offense_description" varchar   NOT NULL,
+    "months_submitted" int   NOT NULL,
+    "year" int   NOT NULL,
+    "offence_count" int   NOT NULL,
+    CONSTRAINT "pk_Offenses" PRIMARY KEY (
+        "county"
+     )
+);
+
+CREATE TABLE "Education" (
+    "county" varchar   NOT NULL,
+    "less_than_high_school" int   NOT NULL,
+    "high_school_diploma" int   NOT NULL,
+    "some_college_or_associates" int   NOT NULL,
+    "bachelors_degree_or_higher" int   NOT NULL,
+    CONSTRAINT "pk_Education" PRIMARY KEY (
+        "county"
+     )
+);
+
+CREATE TABLE "GDP" (
+    "county" varchar   NOT NULL,
+    "year" int   NOT NULL,
+    "gdp" int   NOT NULL,
+    "gdp_rank_in_state" int   NOT NULL,
+    "percent_change_from_preceding_period" decimal(5,2)   NOT NULL,
+    "change_rank_in_state" int   NOT NULL,
+    CONSTRAINT "pk_GDP" PRIMARY KEY (
+        "county"
+     )
+);
+
+ALTER TABLE "Offenses" ADD CONSTRAINT "fk_Offenses_county" FOREIGN KEY("county")
+REFERENCES "Education" ("county");
+
+ALTER TABLE "Education" ADD CONSTRAINT "fk_Education_county" FOREIGN KEY("county")
+REFERENCES "GDP" ("county");
+
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # uw-project-demo
 
+### Presentation
+
+### Github
+
+### Machine Learning
+
 ### Database
 
-read me test
+For our database, we will be creating an RDS PostgreSQL instance that will connect into pgAdmin. To get us started with this, we have created an ERD that will be edited and finalized once our data is finalized and cleaned. The initial ERD and documents are shown below.
+
+ 
